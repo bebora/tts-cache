def7f5d4708f3c41dffd2e24ea358100ca7e30ab
bebora <simone.rizzo@live.com>
2021-05-05 23:00:00 +0200

    Add CORS middleware

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 from threading import Lock
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from app.speech import create_speech_router
 from app.settings import Settings
+from app.speech import create_speech_router
 
 
 def create_app() -> FastAPI:
@@ -14,5 +15,8 @@ def create_app() -> FastAPI:
     settings = Settings()
 
     app.include_router(create_speech_router(audio_lock, counter_lock, settings))
+    app.add_middleware(
+        CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+    )
 
     return app
