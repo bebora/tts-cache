94bc3cc103f0addb3b17fb9ca49e6d52d3f0c375
bebora <simone.rizzo@live.com>
2021-05-05 23:11:59 +0200

    Add function to manage counter

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,12 @@
+import io
+
 from minio import Minio
 from minio.error import S3Error
+from pydantic import BaseModel
+
+
+class MinioCounterTags(BaseModel):
+    working_month: str
 
 
 def object_exists(client: Minio, bucket_name: str, object_name: str) -> bool:
@@ -18,3 +25,24 @@ def obtain_audio(text: str) -> bytes:
     with open("example.mp3", "rb") as fp:
         content = fp.read()
     return content
+
+
+def get_counter_value(
+    client: Minio, bucket_name: str, counter_name: str, working_month: str
+) -> int:
+    tags: MinioCounterTags = client.get_object_tags(bucket_name, counter_name)
+    # Reset counter if a new month started
+    if tags.working_month != working_month:
+        tags.working_month = working_month
+        client.set_object_tags(bucket_name, counter_name, tags)
+        counter_stream = io.BytesIO(b"0")
+        client.put_object(bucket_name, counter_name, counter_stream, len(b"0"))
+        return 0
+    else:
+        try:
+            counter_response = client.get_object(bucket_name, counter_name)
+            counter = int(counter_response.data)
+        finally:
+            counter_response.close()
+            counter_response.release_conn()
+        return counter
